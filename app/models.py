8193a923c2567b352a305a0f8baa96a41d85082c
Mbriito <mariabrito070@mail.com>
2021-02-17 12:58:54 -0300

    first commit

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Todo(models.Model):
+    name = models.CharField(max_length=120)
+    done = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True) #a hora que o documento foi criando
+
+    
+
