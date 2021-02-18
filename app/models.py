214ea9866e48efd719c9e5c87767850347ecdd38
Mbriito <mariabrito070@mail.com>
2021-02-18 20:39:11 -0300

    atualização

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Todo(models.Model):
     name = models.CharField(max_length=120)
     done = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now_add=True) #a hora que o documento foi criando
+    created_at = models.DateTimeField(auto_now_add=True) #a hora que o documento foi criando
 
     
 
