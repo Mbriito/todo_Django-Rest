a109022261257ba569d147e4f2caf8b65174ff01
Mbriito <mariabrito070@mail.com>
2021-02-21 08:40:13 -0300

    detail todo

--- app/views.py
+++ app/views.py
@@ -18,6 +18,28 @@ def todo_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status.HTTP_201_CREATED)  
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)      
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+
+@api_view(['POST', 'GET', 'PUT',]) 
+def todo_detail_change_delete(request, pk):
+    try:
+        todo = Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.metodo=='GET':
+        serializer = TodoSerializer(todo) 
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+               
+
+
+
+
 
 # Create your views here.
